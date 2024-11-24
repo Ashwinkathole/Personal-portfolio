8eb7f218b65be804556e44da08a2f6cf57280ba5
ashwinkathole2001@gmail.com <ashwinkathole2001@gmail.com>
2024-11-23 23:51:37 -0800

    Added javascript for scroll function

---
+++ script.js
@@ -0,0 +1,10 @@
+function right(){
+    const element=document.getElementById("project-container");
+    element.scrollLeft +=50;
+}
+
+function left(){
+    const element=document.getElementById("project-container");
+    element.scrollLeft -=50;
+}
+
